ec339791bac1de357a078679dba1c8858359ea88
Unknown <alexandre.annic1@gmail.com>
2018-06-20 17:02:24 +0200

    transpile with babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+var babelenv = require('babel-preset-env');
 
 module.exports = {
   entry: './lib/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [babelenv]
+          }
+        }
+      }
+    ]
   }
 };
