f0c2c6eeda741089855e464e687f4819c147d99d
Unknown <alexandre.annic1@gmail.com>
2018-06-20 17:24:55 +0200

    Compile to lib

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './lib/index.js',
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
